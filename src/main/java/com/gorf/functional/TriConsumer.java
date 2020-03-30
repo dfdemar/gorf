a48400f9f03dd0ff54ce3520f21c104e7f30d804
David DeMar <dfdemar@gmail.com>
2020-03-30 09:10:59 -0400

    Allow method chaining.

--- src/main/java/com/gorf/functional/TriConsumer.java
+++ src/main/java/com/gorf/functional/TriConsumer.java
@@ -1,10 +1,40 @@
 package com.gorf.functional;
 
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+/**
+ * A three-arity version of {@link BiConsumer}.
+ *
+ * @param <T> the type of the 1st argument to the operation
+ * @param <U> the type of the 2nd argument to the operation
+ * @param <V> the type of the 3rd argument to the operation
+ */
 @FunctionalInterface
-public interface TriConsumer <T, U, V> {
+public interface TriConsumer<T, U, V> {
 
     /**
      * Performs this operation on the given arguments and returns no results.
      */
-    void accept(T var1, U var2, V var3);
+    void accept(T t, U u, V v);
+
+    /**
+     * Returns a composed {@code TriConsumer} that performs, in sequence, this
+     * operation followed by the {@code after} operation. If performing either
+     * operation throws an exception, it is relayed to the caller of the
+     * composed operation. If performing this operation throws an exception,
+     * the {@code after} operation will not be performed.
+     *
+     * @param after the operation to perform after this operation
+     * @return a composed {@code TriConsumer} that performs in sequence this operation followed by the {@code after} operation
+     * @throws NullPointerException if {@code after} is null
+     */
+    default TriConsumer<T, U, V> andThen(TriConsumer<? super T, ? super U, ? super V> after) {
+        Objects.requireNonNull(after);
+
+        return (t, u, v) -> {
+            accept(t, u, v);
+            after.accept(t, u, v);
+        };
+    }
 }
