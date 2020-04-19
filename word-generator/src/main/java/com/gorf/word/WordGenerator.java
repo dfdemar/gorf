d31a107727eba09b9b3f706d98c336ff34d8096b
David DeMar <dfdemar@gmail.com>
2020-04-19 13:25:26 -0400

    Get a random syllable from a list of syllables.

--- word-generator/src/main/java/com/gorf/word/WordGenerator.java
+++ word-generator/src/main/java/com/gorf/word/WordGenerator.java
@@ -1,10 +1,22 @@
 package com.gorf.word;
 
+import java.util.List;
+import java.util.Random;
+
 /**
  * Generates random words based on English syllables.
  */
 public class WordGenerator {
 
-    public void generate() {
+    public void generate(List<String> syllables) {
+        System.out.println(getSyllable(syllables));
+    }
+
+    /**
+     * Returns a random syllable.
+     */
+    private String getSyllable(List<String> syllables) {
+        Random random = new Random();
+        return syllables.get(random.nextInt(syllables.size()));
     }
 }
--- word-generator/src/test/java/com/gorf/word/WordGeneratorTest.java
+++ word-generator/src/test/java/com/gorf/word/WordGeneratorTest.java
@@ -2,11 +2,28 @@ package com.gorf.word;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 class WordGeneratorTest {
+
+    private static final String SYLLABLES_PATH = "D:\\David\\english_syllables.txt";
 
     @Test
     void testGenerate() {
+        List<String> syllables = new ArrayList<>();
+        try (Stream<String> lines = Files.lines(Paths.get(SYLLABLES_PATH))) {
+            lines.collect(Collectors.toCollection(() -> syllables));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         WordGenerator generator = new WordGenerator();
-        generator.generate();
+        generator.generate(syllables);
     }
 }
