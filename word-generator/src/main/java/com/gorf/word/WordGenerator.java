fd5fb68e1321f37678cf60a049712e284970b204
David DeMar <dfdemar@gmail.com>
2020-04-19 13:04:33 -0400

    Create WordGenerator class.

---
+++ word-generator/src/main/java/com/gorf/word/WordGenerator.java
@@ -0,0 +1,10 @@
+package com.gorf.word;
+
+/**
+ * Generates random words based on English syllables.
+ */
+public class WordGenerator {
+
+    public void generate() {
+    }
+}
---
+++ word-generator/src/test/java/com/gorf/word/WordGeneratorTest.java
@@ -0,0 +1,12 @@
+package com.gorf.word;
+
+import org.junit.jupiter.api.Test;
+
+class WordGeneratorTest {
+
+    @Test
+    void testGenerate() {
+        WordGenerator generator = new WordGenerator();
+        generator.generate();
+    }
+}
